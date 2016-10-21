ef1ae3082df2520d246a3142499cc65a20df6f21
Ruchi <ruchiramani@gmail.com>
2016-10-21 11:26:50 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+
 const React = require('react');
 const ReactDOM = require('react-dom');
-
 const Spaceship = require('./components/Spaceship');
 
+
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship name="Millennium Falcon"
+
+  />,
+
   document.getElementById('main')
 );
+
+
 
 require('./test/index-test.js'); // Leave this in!
